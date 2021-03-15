6bff15b0b5f79fd18f54a4abfb32672c89a38e2e
Garett <gpignotti@gmail.com>
2021-03-15 09:37:57 -0700

    added data descripiton link

--- src/get_bike_data.R
+++ src/get_bike_data.R
@@ -1,3 +1,4 @@
 #bike test data
 #can't download via R, need to do manually
 #link to download: https://drive.google.com/drive/folders/1jcpZzFSro4r48y_2JqWVXbZ2rJMByedI
+#data description: https://archive.ics.uci.edu/ml/datasets/Bike+Sharing+Dataset
